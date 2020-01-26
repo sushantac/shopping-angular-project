fe0b51b06e5cd02c4ce6c04a137941d959d566f6
Sushant Chanmanwar <sushant.chanmanwar@gmail.com>
2020-01-26 11:16:59 +1100

    Added component structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { RecipeListComponent } from './recipe-list/recipe-list.component';
+import { ShoppingItemComponent } from './shopping-list/shopping-item/shopping-item.component';
+import { RecipeItemComponent } from './recipe-list/recipe-item/recipe-item.component';
+import { RecipeDetailComponent } from './recipe-list/recipe-item/recipe-detail/recipe-detail.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShoppingListComponent,
+    RecipeListComponent,
+    ShoppingItemComponent,
+    RecipeItemComponent,
+    RecipeDetailComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
